383b392e995a606cfd092d68fc7f47589c82bf70
Caleb Stone <caleblstone@gmail.com>
2021-02-07 19:54:41 -0500

    website update

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let project = document.querySelectorAll("div.project")
 let projectGrid = document.querySelector("div.projectGrid")
-let home = document.querySelector("div.home")
+let home = document.querySelector("div.home header")
 let button = document.querySelectorAll("div.button")
 let expanded = document.querySelectorAll("div.expanded")
 let about = document.querySelector("div.about")
 let contact = document.querySelector("div.contact")
 let bioButtons = document.querySelectorAll("div.bioButton")
+let projectTitle = document.querySelector("div.proj header")
+let aboutT = document.querySelector("div.bioAbout")
+let contactT = document.querySelector("div.bioContact")
 
 home.addEventListener("mouseover", function(){
   home.style.cursor = "pointer"
@@ -16,28 +19,47 @@ home.addEventListener("click", function(){
   expanded.forEach((item, i) => {
     item.style.display = "none"
   });
+  projectTitle.style.display = "none"
+  home.style.textDecoration = "underline"
+  home.innerHTML = "Caleb Stone"
 })
 
 bioButtons[0].addEventListener("click", function(){
   about.style.display = "block"
   contact.style.display = "none"
+  aboutT.style.textDecoration = "underline"
+  contactT.style.textDecoration = "none"
 })
 bioButtons[1].addEventListener("click", function(){
   about.style.display = "none"
   contact.style.display = "block"
+  aboutT.style.textDecoration = "none"
+  contactT.style.textDecoration = "underline"
 })
 
 
 //Changes mouse to pointer when hovering over project
 project.forEach((item, i) => {
   item.addEventListener("mouseover", function(){
-    item.style.cursor = "pointer"
+    // item.style.cursor = "pointer"
   })
   item.addEventListener("click", function(){
-    projectGrid.style.display = "none"
-    let identity = item.id + "Expanded"
-    console.log(identity);
-    let expanded = document.getElementById(identity)
-    expanded.style.display = "flex"
+    if (item.classList.contains("inProgress")) {
+
+    }
+    if (item.classList.contains("done")) {
+      projectGrid.style.display = "none"
+      let identity = item.id + "Expanded"
+      console.log(identity);
+      let expanded = document.getElementById(identity)
+      expanded.style.display = "flex"
+      projectTitle.style.display = "block"
+      let name = item.querySelector("div.name").textContent
+      console.log(name);
+      projectTitle.innerHTML = name
+      projectTitle.style.color = "#FE4818"
+      home.style.textDecoration = "none"
+      home.innerHTML = "●"
+    }
   })
 });
